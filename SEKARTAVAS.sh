755a612c9ec66e1c76015a74284259d18deaae10
SEKAR-TAVAS-CYBER-TEAM <56269625+SEKAR-TAVAS-CYBER@users.noreply.github.com>
2019-10-09 17:00:30 +0700

    SekarTavasCyber
    ©SekarTavasCyber

---
+++ SEKARTAVAS.sh
@@ -0,0 +1,203 @@
+#!/usr/bin/bash/
+#coded by garox
+#SEKAR TAVAS CYBER TEAM
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+echo $i "WELCOME TO TOOLS STCT"
+sleep 4
+clear
+echo $cy "Semoga Anda Senang Dengan Project Kami:)"
+sleep 4
+clear
+echo $pu "OKEH LANGSUNG GASKUN MHAMHANK"
+sleep 4
+clear
+echo $me "  _______ _________ _______ _________"
+echo $me " (  ____ \\__   __/(  ____ \\__   __/"
+echo $me " | (    \/   ) (   | (    \/   ) ("
+echo $me " | (_____    | |   | |         | |"
+echo $me " (_____  )   | |   | |         | |"
+echo $pu "       ) |   | |   | |         | |"
+echo $pu " /\____) |   | |   | (____/\   | |"
+echo $pu " \_______)   )_(   (_______/   )_("
+echo ""
+echo $cy "________________________________________________________"
+echo $ku "      -[×]TOOLS VERSION 0.0.0.0.1[×]-"
+echo $ku "      -[√]SEKAR TAVAS CYBER TEAM[√]-"
+echo $ku "      -[√]Coded by :G4R0X[√]"
+echo $ku "      -[√]SPECIAL THANKS TO ALL MEMBER&ADMIN[√]"
+echo $cy "________________________________________________________"
+echo $ku "▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀"
+echo $me ""
+echo $pu " [0]"$ku"EXIT TOOLS"
+echo $cy "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo $me " [1]"$pu"INSTALL BAHAN BREW:/"
+echo $cy "///////////////////////////"
+echo $me "                            [2]"$pu"SPAM BRUTAL:/"
+echo $cy "                           /////////////////////////////"
+echo $me " [3]"$pu"TOOLS-Mr.F4K4"
+echo $cy "///////////////////////////"
+echo $me "                            [4]"$pu"TROJANS ATTACK"
+echo $cy "                           /////////////////////////////"
+echo $me " [5]"$pu"LUCITA DDOS"
+echo $cy "///////////////////////////"
+echo $me "                            [6]"$pu"TUANB4DUT TOOLS"
+echo $cy "                           /////////////////////////////"
+echo $me " [7]"$pu"BAJINGAN TOOLS"
+echo $cy "///////////////////////////"
+echo $me "                            [8]"$pu"BUCINTOOLS"
+echo $cy "                           /////////////////////////////"
+echo $me " [9]"$pu"LITESPAM"
+echo $cy "///////////////////////////"
+echo $me "                            [10]"$pu"RUSMANA TOOLS"
+echo $cy "                           /////////////////////////////"
+echo $me " [11]"$pu"DAFTAR ANGGOTA STCT"
+echo $cy "///////////////////////////"
+echo $bi "╭─"$i"Pilih Salah Satu"
+read -p " ╰─────────$STCT>>"  pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg install git
+pkg install python2
+pkg install figlet
+pkg update && pkg upgrade
+pkg install wget
+pkg install bash
+pip2 install mechanize
+pip2 install requests
+sh S.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/LuciferXtools/404
+cd 404
+sh TapselAX.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/JustAHackers/perusak
+cd perusak
+python2 perusak.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+python2 pukul.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+sh TUANB4DUT..sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/MrGameOver16/InstallBucin
+cd InstallBucin
+sh bucin.sh
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet Tunggu | lolcat
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+echo ""
+echo ""
+echo ""
+echo $me"NANTI AKU KASIH TAU:V"
+sleep 5
+exit
+fi
+
+if [ $pil = 0 ]
+then
+figlet Terima Kasih | lolcat
+echo $bi "ADA KESALAHAN KONTAK SAYA SAJA SAYA"
+echo $bi "OKE BYEBYE"
+fi
+
+if [ $pil = ]
+then
+echo $me "PILIH YANG BENAR SAYANGKU:!!!"
+sleep 5
+sh S.sh
+fi
